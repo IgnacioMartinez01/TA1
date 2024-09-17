c9719ee5361aadac6250187c23b1a6b000f1ebdb
IgnacioMartinez01 <igmartinezpintosgmail.com>
2024-09-17 14:45:34 -0300

    Done

--- EntregasReactTAs/react-example/src/App.js
+++ EntregasReactTAs/react-example/src/App.js
@@ -18,11 +18,34 @@ function App() {
         >
           Learn React
         </a>
-        
+        <h1>Parte 1</h1>
         <Card Titulo={"Card 1"} Descripcion={'Descripcion de la card 1'}Persona={' Ignacio'} Estilo="lightcoral"/>
         <Card Titulo={"Card 2"} Descripcion={'Descripcion de la card 2'}Persona={' Martin'} Estilo="lightgrey"/>    
         <Card Titulo={"Card 3"} Descripcion={'Descripcion de la card 3'}Persona={' Agustin'} Estilo="blue"/>    
-      
+        <h1>Parte 2</h1>
+        <Card Estilo="lightblue">
+        <h1>Card 1</h1>
+        <p>Descripción 1</p>
+        <p>Asignado a: Ignacio</p>
+        <p>Fecha de inicio: 01/01/2024</p>
+        <p>Fecha de finalización: 12/12/2024</p>
+        </Card>
+
+        <Card Estilo="lightgreen">
+        <h1>Card 2</h1>
+        <p>Descripción 2</p>
+        <p>Asignado a: Esteban</p>
+        <p>Fecha de inicio: 02/01/2024</p>
+        <p>Fecha de finalización: 12/12/2024</p>
+        </Card>
+
+        <Card Estilo="lightgrey">
+        <h1>Card 3</h1>
+        <p>Descripción 3</p>
+        <p>Asignado a: Agustin</p>
+        <p>Fecha de inicio: 01/11/2024</p>
+        <p>Fecha de finalización: 02/12/2024</p>
+        </Card>
       </header>
     </div>
   );
--- EntregasReactTAs/react-example/src/Card.js
+++ EntregasReactTAs/react-example/src/Card.js
@@ -1,4 +1,9 @@
+import { Children } from "react";
 import ESTILOS from "./Card.module.css";
+
+/* PARTE 1*/ 
+
+/*
 function Card({Titulo,Descripcion,Persona,FechaI,FechaF,Estilo}){
     return (
         <div style={{
@@ -15,6 +20,20 @@ function Card({Titulo,Descripcion,Persona,FechaI,FechaF,Estilo}){
             <p>{FechaI}</p>
             <p>{FechaF}</p>
         </div>
+    )
+}
+*/
+function Card ({Estilo,children}){
+    return(
+        <div style={{
+            backgroundColor:Estilo,
+            borderRadius:50,
+            padding: 20,
+            margin: 14,
+            maxwidth: 300,
+        }}>
+           {children}
+           </div>
     )
 }
 
