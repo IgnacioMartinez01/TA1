31b5dfe2aa91378656889e51db16b94c20db2bc1
IgnacioMartinez01 <igmartinezpintosgmail.com>
2024-09-22 05:41:48 -0300

    done

---
+++ EntregasReactTAs/ta6/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Componente from './Components/Componente';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Componente/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ EntregasReactTAs/ta6/src/Components/Componente.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function Componente() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={newTask}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleAddTask}>Agregar Tarea</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Componente;
