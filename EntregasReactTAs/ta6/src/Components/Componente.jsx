e2fe81e375afbe37b8235ae43525f03d73b73ac1
IgnacioMartinez01 <igmartinezpintosgmail.com>
2024-09-22 13:46:31 -0300

    Done

--- EntregasReactTAs/ta6/src/Components/Componente.jsx
+++ EntregasReactTAs/ta6/src/Components/Componente.jsx
@@ -1,18 +1,45 @@
+
 import React, { useState } from 'react';
 
-function Componente() {
+function TaskList() { 
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
+  const [editingTaskId, setEditingTaskId] = useState(null);
 
-  const handleInputChange = (event) => {
+  const   
+ handleInputChange = (event) => {
     setNewTask(event.target.value);
   };
 
   const handleAddTask = () => {
     if (newTask.trim() !== '') {
-      setTasks([...tasks, newTask]);
+      setTasks([...tasks, { id: Date.now(),   
+ task: newTask, isEditing: false }]);
       setNewTask('');
     }
+  };
+
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter(task => task.id !== taskId));
+  };
+
+  const handleEditTask = (taskId) => {
+    setEditingTaskId(taskId);
+  };
+
+  const handleSaveTask = (taskId) => {
+    if (newTask.trim() !== '') {
+      setTasks(tasks.map(task =>
+        task.id === taskId ? { ...task, task: newTask, isEditing: false } : task
+      ));
+      setEditingTaskId(null);
+      setNewTask('');
+    }
+  };
+
+  const handleCancelEdit = (taskId) => {
+    setEditingTaskId(null);
+    setNewTask(tasks.find(task => task.id === taskId).task); 
   };
 
   return (
@@ -21,15 +48,32 @@ function Componente() {
         type="text"
         value={newTask}
         onChange={handleInputChange}
+        placeholder="Nueva tarea"
       />
-      <button onClick={handleAddTask}>Agregar Tarea</button>
+      <button onClick={handleAddTask}>Agregar</button>
       <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
+      {tasks.map((task, index) => ( 
+    <li key={task.id}>
+      {task.id === editingTaskId ? (
+        <input
+          type="text"
+          value={newTask}
+          onChange={handleInputChange}
+          onBlur={() => handleSaveTask(task.id)}
+        />
+      ) : (
+        <span>{task.task}</span>
+      )}
+      <button onClick={() => handleEditTask(task.id)}>Editar</button>
+      {task.id === editingTaskId && (
+        <button onClick={() => handleCancelEdit(task.id)}>Cancelar</button>
+      )}
+      <button onClick={() => removeTask(task.id)}>Eliminar</button> 
+    </li>
+  ))}
       </ul>
     </div>
   );
 }
 
-export default Componente;
+export default TaskList;
